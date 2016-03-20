0da22e9d31405ef8a307814a6b24aea52a4ecb47
Dan Gillespie <dan@redspread.com>
2016-03-20 05:18:49 -0700

    used docker networking IP address for DNS server

--- image/start.sh
+++ image/start.sh
@@ -9,4 +9,8 @@ weave launch-proxy --without-dns --rewrite-inspect
 # add localkube to network
 weave expose -h "localkube.weave.local"
 
+# setup SkyDNS to use docker networking IP
+ip=$(ip -4 addr show dev docker0 | grep -m1 -o 'inet [.0-9]*' | sed 's/inet \([.0-9]*\)/\1/')
+export DNS_SERVER=$ip:1970
+
 localkube start
